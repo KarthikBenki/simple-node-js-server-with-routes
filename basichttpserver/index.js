49d2a9bec0fd41557f2eaa70bdfd575a60bef085
KarthikBenki <karthikmc007@gmail.com>
2023-06-28 15:51:34 +0530

    use of res.writehead

--- basichttpserver/index.js
+++ basichttpserver/index.js
@@ -9,6 +9,7 @@ const server = http.createServer((req,res)=>{
     console.log(req.url);
 
     if (req.url==="/login") {
+        res.writeHead(200,{'content-type':"text/html"})
          res.end('<h1>Hello World</h1>')
          return
     }
