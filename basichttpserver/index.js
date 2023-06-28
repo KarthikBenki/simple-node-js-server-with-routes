6fd73ed4b56269e675cbc1427f87986095a63db6
KarthikBenki <karthikmc007@gmail.com>
2023-06-28 18:38:24 +0530

    update start script

--- basichttpserver/index.js
+++ basichttpserver/index.js
@@ -5,46 +5,91 @@ const fs = require('fs')
 //usique address of server
 const port = 8000;
 
-const server = http.createServer((req,res)=>{
-    //request handler function
+// const server = http.createServer((req,res)=>{
+//     //request handler function
+//     console.log(req.url)
+
+//     if (req.url==="/login") {
+//         res.writeHead(200,{'content-type':"text/html"})
+//         fs.readFile('./login.html',(err,data)=>{
+//             if (err) {
+//                 console.log(err);
+//                 res.end('<h1>Error!</h1>')
+//             }
+//             res.end(data)
+//         })
+         
+//          return
+//     }
+
+//     if (req.url==="/home" || req.url==="/") {
+//         res.writeHead(200,{'content-type':"text/html"})
+//         fs.readFile('./home.html',(err,data)=>{
+//             if (err) {
+//                 console.log(err);
+//                 res.end('<h1>Error!</h1>')
+//             }
+//             res.end(data)
+//         })
+         
+//          return
+//     }
+
+//     res.writeHead(404,{'content-type':"text/html"})
+ 
+//    fs.readFile('./_404.html',(err,data)=>{
+//     if (!err && req.method=="GET"){
+//             res.end(data)
+//             return
+//     }
+//     res.end('<h1>Error! Server busy with some other task</h1>')
+//    })
+// })
+
+const server = http.createServer((req, res) => {
+    // Request handler function
     console.log(req.url);
 
-    if (req.url==="/login") {
-        res.writeHead(200,{'content-type':"text/html"})
-        fs.readFile('./login.html',(err,data)=>{
-            if (err) {
-                console.log(err);
-                res.end('<h1>Error!</h1>')
-            }
-            res.end(data)
-        })
-         
-         return
-    }
+    switch (req.url) {
+        case "/login":
+            res.writeHead(200, { 'content-type': "text/html" });
+            fs.readFile('./login.html', (err, data) => {
+                if (err) {
+                    console.log(err);
+                    res.end('<h1>Error!</h1>');
+                } else {
+                    res.end(data);
+                }
+            });
+            break;
 
-    if (req.url==="/home" || req.url==="/") {
-        res.writeHead(200,{'content-type':"text/html"})
-        fs.readFile('./home.html',(err,data)=>{
-            if (err) {
-                console.log(err);
-                res.end('<h1>Error!</h1>')
-            }
-            res.end(data)
-        })
-         
-         return
-    }
+        case "/home":
+        case "/":
+            res.writeHead(200, { 'content-type': "text/html" });
+            fs.readFile('./home.html', (err, data) => {
+                if (err) {
+                    console.log(err);
+                    res.end('<h1>Error!</h1>');
+                } else {
+                    res.end(data);
+                }
+            });
+            break;
 
-    res.writeHead(404,{'content-type':"text/html"})
- 
-   fs.readFile('./_404.html',(err,data)=>{
-    if (!err && req.method=="GET"){
-            res.end(data)
-            return
+        default:
+            res.writeHead(404, { 'content-type': "text/html" });
+            fs.readFile('./_404.html', (err, data) => {
+                if (err) {
+                    console.log(err);
+                    res.end('<h1>Error! Server busy with some other task</h1>');
+                } else {
+                    res.end(data);
+                }
+            });
+            break;
     }
-    res.end('<h1>Error! Server busy with some other task</h1>')
-   })
-})
+});
+
 
 server.listen(port,(err)=>{
     if(err){
